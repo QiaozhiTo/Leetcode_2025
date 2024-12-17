a9ca2f8c11467308ca6091c5bb1f7f42e25a0c08
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2024-12-17 07:35:45 -0500

    Time: 40 ms (10.88%), Space: 26.5 MB (92.02%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,10 +1,18 @@
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         count = {}
+#         for num in nums:
+#             count[num] = 1 + count.get(num,0)
+#         for key, val in count.items():
+#             if val >1:
+#                 return True
+#         return False
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        count = {}
-        for num in nums:
-            count[num] = 1 + count.get(num,0)
-        for key, val in count.items():
-            if val >1:
+        nums.sort()
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
                 return True
         return False
-        
+            

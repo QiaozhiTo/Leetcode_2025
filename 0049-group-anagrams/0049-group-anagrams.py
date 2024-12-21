24a7e8bff1d4a780d7717aec1caf625b5d209464
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2024-12-21 16:03:17 -0500

    Time: 9 ms (88.04%), Space: 20.8 MB (40.14%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,11 +1,20 @@
+# class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         res = defaultdict(list)
+#         for string in strs:
+#             count = [0] * 26
+#             for c in string:
+#                 count[ord('a') - ord(c)] += 1
+#             res[tuple(count)].append(string)
+#         return list(res.values())
+    
+# lists are mutable and cannot be used as keys in a dictionary, whereas tuples are immutable and can be used as keys.
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         res = defaultdict(list)
-        for string in strs:
-            count = [0] * 26
-            for c in string:
-                count[ord('a') - ord(c)] += 1
-            res[tuple(count)].append(string)
+        for s in strs:
+            sortedS = ''.join(sorted(s))
+            res[sortedS].append(s)
         return list(res.values())
-    
-# lists are mutable and cannot be used as keys in a dictionary, whereas tuples are immutable and can be used as keys.
+        

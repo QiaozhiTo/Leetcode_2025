0409a42ff9ac1a35ed9c07d6a232d7c751f40e7f
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2024-12-21 13:17:53 -0500

    Time: 2 ms (75.47%), Space: 51.9 MB (14.42%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,11 +5,14 @@
  */
 var twoSum = function(nums, target) {
     const prevMap = new Map();
+    
     for (let i = 0; i < nums.length; i ++){
         let diff = target - nums[i]
+        
         if (prevMap.has(diff)){
             return [prevMap.get(diff), i];
         }
+        
         else{
             prevMap.set(nums[i], i);
         }

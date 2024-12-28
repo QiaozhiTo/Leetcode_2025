375373a2c125634f11a6332ef9925cb1384d844b
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2024-12-28 14:02:59 -0500

    Time: 4 ms (84.21%), Space: 12.4 MB (15.92%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,20 +1,22 @@
-class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
         rows = defaultdict(set)
         cols = defaultdict(set)
-        spread = defaultdict(set)
+        squares = defaultdict(set)
         for r in range(9):
             for c in range(9):
                 if board[r][c] == '.':
                     continue
-                if (board[r][c] in rows[r]) or (board[r][c] in cols[c]) or (board[r][c] in spread[(r//3, c//3)]):
+                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r//3, c//3)]:
                     return False
                 rows[r].add(board[r][c])
                 cols[c].add(board[r][c])
-                spread[(r//3,c//3)].add(board[r][c])
+                squares[(r//3, c//3)].add(board[r][c])
         return True
-                
-                
-        
-        
+
+
         

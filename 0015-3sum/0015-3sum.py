47ca602a1f50b1380866233cd9942bdcb47d2a4c
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2024-12-29 10:59:25 -0500

    Time: 734 ms (65.52%), Space: 18.5 MB (21.94%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -6,8 +6,8 @@ class Solution(object):
         """
         nums.sort()
         res = []
-
         for i in range(len(nums)-2):
+            # not having duplicates 
             if nums[i-1] == nums[i] and i > 0:
                 continue
             l, r = i + 1, len(nums)-1
@@ -20,6 +20,11 @@ class Solution(object):
                 else:
                     res.append([nums[i], nums[l], nums[r]])
                     l += 1
+                    # not having duplicates 
                     while l < r and nums[l] == nums[l-1]:
                         l += 1
         return res
+
+# time complexity: O(N^2) + O(nlogn) -> O(N^2)[two nested array]
+# space complexity: could be O(1) or O(N) sorting does take some extra spaces
+

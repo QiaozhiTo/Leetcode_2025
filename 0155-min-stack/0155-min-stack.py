0e945ac538ccc2568e3b0433ebbb37f0e37d8452
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2025-01-01 13:19:39 -0500

    Time: 7 ms (75.49%), Space: 15.8 MB (35.64%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,28 +1,77 @@
-class MinStack:
+# two stacks
+class MinStack(object):
 
     def __init__(self):
         self.stack = []
+        self.ministack = []
         
-    def push(self, val: int) -> None:
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
         self.stack.append(val)
-
-    def pop(self) -> None:
-        self.stack.pop()
-
-    def top(self) -> int:
-        return self.stack[-1]
-
-    def getMin(self) -> int:
-        tmp = []
-        mini = self.stack[-1]
-        while self.stack:
-            mini = min(mini, self.stack[-1])
-            tmp.append(self.stack.pop())
-        while tmp:
-            self.stack.append(tmp.pop())
-        return mini
+        self.ministack.append(min(val, self.ministack[-1] if self.ministack else val))
         
 
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack.pop()
+        self.ministack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.ministack[-1]
+        
+# Method 2 Brute Force
+
+# class MinStack(object):
+
+#     def __init__(self):
+#         self.stack = []        
+
+#     def push(self, val):
+#         """
+#         :type val: int
+#         :rtype: None
+#         """
+#         self.stack.append(val)        
+
+#     def pop(self):
+#         """
+#         :rtype: None
+#         """
+#         self.stack.pop()
+#     def top(self):
+#         """
+#         :rtype: int
+#         """
+#         return self.stack[-1]
+        
+#     def getMin(self):
+#         """
+#         :rtype: int
+#         """
+#         tmp = []
+#         mini = self.stack[-1]
+#         while self.stack:
+#             mini = min(mini, self.stack[-1])
+#             tmp.append(self.stack.pop())
+#         while tmp:
+#             self.stack.append(tmp.pop())
+#         return mini
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
@@ -30,3 +79,5 @@ class MinStack:
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
+
+

177fc6c9d6058d621f17096d76c15b775ee9f60b
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2025-10-19 13:37:08 -0400

    Time: 108 ms (42.44%), Space: 51.3 MB (55.73%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -4,9 +4,11 @@ class Solution:
         res = 0
         prevEnd = intervals[0][1]
         for start, end in intervals[1:]:
+            # not overlapping with next, update the prevEnd
             if prevEnd <= start:
                 prevEnd = end
             else:
+            # overlapping, remove the one with larger end(the longer interval)
                 res += 1
                 prevEnd = min(prevEnd, end)
         return res

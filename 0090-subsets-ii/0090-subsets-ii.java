d408d02f75977a247901f7cbf3d75d636ba1f63a
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2025-10-29 18:54:14 -0400

    Time: 1 ms (99.96%), Space: 43.7 MB (29.81%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
+        Arrays.sort(nums);
+        dfs(nums, 0, subset, res);
+        return res;
+        
+        
+    }
+
+    private void dfs(int[] nums, int i, List<Integer> subset, List<List<Integer>> res){
+        if(i == nums.length){
+            res.add(new ArrayList<>(subset));
+            return ;
+        }
+        subset.add(nums[i]);
+        dfs(nums, i + 1, subset, res);
+        subset.remove(subset.size()-1);
+        while(i+1 < nums.length && nums[i+1] == nums[i]){
+            i++;
+        }
+        dfs(nums, i +1, subset, res);
+
+
+    }
+}

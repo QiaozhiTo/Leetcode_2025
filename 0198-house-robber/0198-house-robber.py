740bb647cc7e3dd72e0ef768bc0c283e14d05627
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2025-11-04 22:56:57 -0500

    Time: 0 ms (100%), Space: 17.5 MB (99.68%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,5 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        for i in range(len(nums)-3, -1, -1):
-            nums[i] += max(nums[i+2:])
-        return max(nums)
+        rob1, rob2 = 0, 0
+        # rob1 rob2 1 2 3 1
+        for n in nums:
+            tmp = max(rob1+n, rob2)
+            rob1 = rob2
+            rob2 = tmp
+        return rob2
+
+# class Solution:
+#     def rob(self, nums: List[int]) -> int:
+#         for i in range(len(nums)-3, -1, -1):
+#             nums[i] += max(nums[i+2:])
+#         return max(nums)

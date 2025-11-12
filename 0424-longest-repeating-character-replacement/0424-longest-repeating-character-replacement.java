4700c98e31d97029452363416233576d2d6afbc5
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2025-11-12 10:41:02 -0500

    Time: 26 ms (29.55%), Space: 47.3 MB (5.33%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,17 +1,40 @@
+// class Solution {
+//     public int characterReplacement(String s, int k) {
+//         Map<Character, Integer> count = new HashMap<>();
+//         int l = 0;
+//         int res = 0;
+//         for (int r = 0; r < s.length(); r ++){
+//             char c = s.charAt(r);
+//             count.put(c, count.getOrDefault(c, 0)+1);
+//             while ((r - l + 1) - Collections.max(count.values()) > k){
+//                 char left = s.charAt(l);
+//                 count.put(left, count.getOrDefault(left, 0)-1);
+//                 l ++;
+//             }
+//             res = Math.max(res, r - l + 1);
+//         }
+//         return res;
+//     }
+// }
+
+// optimization
 class Solution {
     public int characterReplacement(String s, int k) {
         Map<Character, Integer> count = new HashMap<>();
         int l = 0;
         int res = 0;
-        for (int r = 0; r < s.length(); r ++){
+        int MaxCount = 0;
+
+        for (int r =0; r < s.length(); r++){
             char c = s.charAt(r);
             count.put(c, count.getOrDefault(c, 0)+1);
-            while ((r - l + 1) - Collections.max(count.values()) > k){
+            MaxCount = Math.max(MaxCount, count.get(c));
+            while( (r-l+1) - MaxCount > k){
                 char left = s.charAt(l);
                 count.put(left, count.getOrDefault(left, 0)-1);
                 l ++;
             }
-            res = Math.max(res, r - l + 1);
+            res = Math.max(res, r-l+1);
         }
         return res;
     }

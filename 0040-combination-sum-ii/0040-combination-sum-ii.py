dfe20067e6e1eede590bda87f485ad0200ba92a4
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2025-11-17 13:00:04 -0500

    Time: 26 ms (33.56%), Space: 18 MB (14.01%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,24 +1,24 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-
         res = []
         candidates.sort()
-        
-        def generate_susbet(i, cur, total):
+
+        def dfs(i, curr, total):
             if total == target:
-                res.append(cur.copy())
+                res.append(curr[:])
                 return 
             if i == len(candidates) or total > target:
                 return 
-        
-            cur.append(candidates[i])
-            generate_susbet(i+1, cur, total + candidates[i])
-        
-            cur.pop()
-            while i+1 < len(candidates) and candidates[i] == candidates[i+1]:
+            
+            curr.append(candidates[i])
+            dfs(i+1, curr, total + candidates[i])
+            while i + 1 < len(candidates) and candidates[i+1] == candidates[i]:
                 i += 1
-
-            generate_susbet(i+1, cur, total)
-        generate_susbet(0, [], 0)
-
+            curr.pop()
+           
+            dfs(i+1, curr, total)
+        dfs(0, [], 0)
         return res
+
+                 
+        

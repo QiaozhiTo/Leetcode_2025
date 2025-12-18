cb47e0ed685ed711cbd23620b1a941ff73ccfbe6
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2025-12-17 23:25:03 -0500

    Time: 31 ms (30.99%), Space: 17.7 MB (99.57%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,67 +1,50 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        # breadth first search     
+        rows, cols = len(grid), len(grid[0])
+        directions = [[0,1],[0,-1],[1,0],[-1,0]]
+        visit = set()
+        area = 0
+
+        def bfs(r, c):
+            q = deque([(r, c)])
+            visit.add((r,c))
+            res = 1
+
+            while q:
+                r, c = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    if (nr in range(rows) and nc in range(cols) and
+                    (nr, nc) not in visit and grid[nr][nc] == 1):
+                        q.append((nr, nc))
+                        visit.add((nr, nc))
+                        res += 1
+            return res
+
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1 and (r, c) not in visit:
+                    area = max(area, bfs(r,c))
+        return area
+  
 # class Solution:
 #     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-#         # depth first search
 #         rows, cols = len(grid), len(grid[0])
 #         visit = set()
 #         area = 0
 
 #         def dfs(r, c):
-#             if (r < 0 or c < 0 or r >= rows or c >= cols 
-#                 or (r,c) in visit or grid[r][c] == 0):
-#                 # can't use continue here, why??
+#             if (r < 0 or c < 0 or r == rows or c == cols or 
+#                 grid[r][c] == 0 or (r,c) in visit):
 #                 return 0
-#             visit.add((r,c)) 
-#             return 1 + (dfs(r+1, c) + dfs(r-1, c) + dfs(r, c+1) +dfs(r, c-1) )
-#         for r in range(rows):
-#             for c in range(cols):
-#                 area = max(area, dfs(r,c))
-#         return area
-
-# class Solution:
-#     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-#         # breadth first search     
-#         rows, cols = len(grid), len(grid[0])
-#         visit = set()
-#         area = 0   
-#         def dfs(r, c):
-#             q = deque()
-#             res = 0
-#             q.append((r,c))
 #             visit.add((r,c))
-#             while q:
-#                 res += 1
-#                 row, col = q.popleft()
-#                 directions = [[0,1], [0,-1],[1,0], [-1,0]]
-#                 for dr, dc in directions:
-#                     r, c = row + dr, col + dc
-#                     if (r < 0 or c < 0 or r == rows or c == cols 
-#                         or (r,c) in visit or grid[r][c]== 0):
-#                         continue
-#                     q.append((r,c))
-#                     visit.add((r,c))
-#             return res
+#             return 1 + dfs(r+1, c) + dfs(r-1, c) + dfs(r, c+1) + dfs(r, c-1)
+
 
 #         for r in range(rows):
 #             for c in range(cols):
-#                 if grid[r][c] == 1 and (r,c) not in visit:
-#                     area = max(area, dfs(r,c))
+#                 if grid[r][c] == 1:
+#                     area = max(area, dfs(r, c))
 #         return area
-class Solution:
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        rows, cols = len(grid), len(grid[0])
-        visit = set()
-        area = 0
-
-        def dfs(r, c):
-            if (r < 0 or c < 0 or r == rows or c == cols or 
-                grid[r][c] == 0 or (r,c) in visit):
-                return 0
-            visit.add((r,c))
-            return 1 + dfs(r+1, c) + dfs(r-1, c) + dfs(r, c+1) + dfs(r, c-1)
-
-
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 1:
-                    area = max(area, dfs(r, c))
-        return area

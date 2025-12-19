e63b66a3cd32104b5ccce944954d1f02452d8765
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2025-12-18 21:45:46 -0500

    Time: 19 ms (71.63%), Space: 17.6 MB (100%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,32 +1,28 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         # breadth first search     
-        rows, cols = len(grid), len(grid[0])
-        directions = [[0,1],[0,-1],[1,0],[-1,0]]
+        directions =[[0, 1], [0, -1], [1, 0], [-1, 0]]
         visit = set()
+        rows, cols = len(grid), len(grid[0])
         area = 0
-
         def bfs(r, c):
             q = deque([(r, c)])
-            visit.add((r,c))
+            visit.add((r, c))
             res = 1
-
             while q:
-                r, c = q.popleft()
+                row, col = q.popleft()
                 for dr, dc in directions:
-                    nr, nc = r + dr, c + dc
-                    if (nr in range(rows) and nc in range(cols) and
-                    (nr, nc) not in visit and grid[nr][nc] == 1):
-                        q.append((nr, nc))
-                        visit.add((nr, nc))
-                        res += 1
+                    nr, nc = dr + row, dc + col
+                    if (nr < 0 or nc < 0 or nr == rows or nc == cols or (nr, nc) in visit or grid[nr][nc]== 0):
+                        continue
+                    q.append((nr, nc))
+                    visit.add((nr, nc))
+                    res += 1
             return res
-
-
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1 and (r, c) not in visit:
-                    area = max(area, bfs(r,c))
+                    area = max(area, bfs(r, c))
         return area
   
 # class Solution:

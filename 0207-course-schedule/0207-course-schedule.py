8577b54c1dbcbd9181c6ecba0fb668e3120a5b43
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2025-12-18 23:04:12 -0500

    Time: 8 ms (22.47%), Space: 18.8 MB (91.96%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,9 +1,9 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         preMap = {i : [] for i in range(numCourses)}
-        visiting = set()
         for crs, pre in prerequisites:
             preMap[crs].append(pre)
+        visiting = set()
 
         def dfs(crs):
             if crs in visiting:
@@ -18,7 +18,8 @@ class Solution:
             preMap[crs] = []
             return True
 
+
         for crs in range(numCourses):
             if not dfs(crs):
                 return False
-        return True
+        return True     

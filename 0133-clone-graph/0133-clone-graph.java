7020080080ac90c31df470235587fd7a58f5f0d8
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2025-12-20 13:59:18 -0500

    Time: 25 ms (70.96%), Space: 44.5 MB (10.9%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -30,6 +30,7 @@ class Solution {
         if(oldToNew.containsKey(node)){
             return oldToNew.get(node);
         }
+        
         Node copy = new Node(node.val);
         oldToNew.put(node, copy);
 

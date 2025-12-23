fa17f738aafe4681ec82e7f1f169e85f7efe6428
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2025-12-23 10:36:32 -0500

    Time: 33 ms (74.04%), Space: 20.5 MB (59.71%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,18 +1,18 @@
 # Floyd Warshall Algorithm
-# class Solution:
-#     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        # reach = [[False] * numCourses for _ in range(numCourses)]
-        # for u, v in prerequisites:
-        #     reach[u][v] = True
-        # for i in range(numCourses):
-        #     for j in range(numCourses):
-        #         if reach[j][i]: # j is prerequisite of i
-        #             for k in range(numCourses):
-        #                 if reach[i][k]:# i is prerequistite of k
-        #                     reach[j][k] = True # then j is reqrequisite of k
-        # return [reach[u][v] for u, v in queries]
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        reach = [[False] * numCourses for _ in range(numCourses)]
+        for u, v in prerequisites:
+            reach[u][v] = True
+        for i in range(numCourses):
+            for j in range(numCourses):
+                if reach[j][i]: # j is prerequisite of i
+                    for k in range(numCourses):
+                        if reach[i][k]:# i is prerequistite of k
+                            reach[j][k] = True # then j is reqrequisite of k
+        return [reach[u][v] for u, v in queries]
 
-# depth first search
+### depth first search
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
         adj = collections.defaultdict(list)
@@ -24,6 +24,7 @@ class Solution:
                 prereqMap[crs] = set()
                 for prereq in adj[crs]:
                     prereqMap[crs].update(dfs(prereq))
+                    # prereqMap[crs] = prereqMap[crs] | dfs(prereq)
                 prereqMap[crs].add(crs)
             return prereqMap[crs]
 

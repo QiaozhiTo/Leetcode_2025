df4c3047c621b9891a177f925fcece3002a06648
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2025-12-25 17:04:23 -0500

    Time: 34 ms (73.31%), Space: 20.6 MB (22.81%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,4 +1,5 @@
 # Floyd Warshall Algorithm
+'''
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
         reach = [[False] * numCourses for _ in range(numCourses)]
@@ -32,6 +33,26 @@ class Solution:
         for crs in range(numCourses):
             dfs(crs)
         
+        res = []
+        for u, v in queries:
+            res.append(u in prereqMap[v])
+        return res
+'''
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        adj = collections.defaultdict(list)
+        for u, v in prerequisites:
+            adj[v].append(u)
+        def dfs(crs):
+            if crs not in prereqMap:
+                prereqMap[crs] = set()
+                for pre in adj[crs]:    
+                    prereqMap[crs].update(dfs(pre))
+                prereqMap[crs].add(crs)
+            return prereqMap[crs]
+        prereqMap = {}
+        for crs in range(numCourses):
+            dfs(crs)
         res = []
         for u, v in queries:
             res.append(u in prereqMap[v])

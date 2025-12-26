15b12062316549d93dce53dff52857c027615f86
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2025-12-26 09:15:42 -0500

    Time: 82 ms (20.23%), Space: 23.7 MB (32.82%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -6,25 +6,25 @@ class Solution:
         rows, cols = len(rooms), len(rooms[0])
         visit = set()
         q = deque()
-        def addroom(r, c):
-            if (r < 0 or c < 0 or r ==rows or c ==cols or rooms[r][c] == -1 or (r,c) in visit):
-                return 
-            q.append((r, c))
-            visit.add((r, c))
 
+        def addroom(r, c):
+            if (r in range(rows) and c in range(cols) and (r, c) not in visit and rooms[r][c] != -1):
+                q.append((r, c))
+                visit.add((r, c))
+        
         for r in range(rows):
             for c in range(cols):
                 if rooms[r][c] == 0:
                     q.append((r, c))
                     visit.add((r, c))
         dist = 0
-
         while q:
             for i in range(len(q)):
                 row, col = q.popleft()
                 rooms[row][col] = dist
-                addroom(row, col+1)
-                addroom(row, col-1)
-                addroom(row+1, col)
-                addroom(row-1, col)
+                addroom(row, col + 1)
+                addroom(row, col - 1)
+                addroom(row + 1, col)
+                addroom(row - 1, col)
             dist += 1
+

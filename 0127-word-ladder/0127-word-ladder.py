2ef730b54522e193ef01580f931ddc495f41ebf4
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2026-01-04 10:25:31 -0500

    Time: 43 ms (93.13%), Space: 20.8 MB (45.12%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,61 +1,29 @@
-"""class Solution:
-    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        
-        if endWord not in wordList:
-            return 0
-        wordList.append(beginWord)
-        nei_map = collections.defaultdict(list)
-        for word in wordList:
-            for i in range(len(word)):
-                pattern = word[:i] + "*" + word[i+1:]
-                nei_map[pattern].append(word)
-        q = deque([beginWord])
-        visit = set([beginWord])
-        res = 1
-        
-        while q:
-            for i in range(len(q)):
-                word = q.popleft()
-                if word == endWord:
-                    return res
-                for i in range(len(word)):
-                    pattern = word[:i] + "*" + word[i+1:]
-                    for nei in nei_map[pattern]:
-                        if nei not in visit:
-                            q.append(nei)
-                            visit.add(nei)
-            res += 1
-        return 0   
-"""        
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         if endWord not in wordList:
             return 0
-        neiMap = collections.defaultdict(list)
         wordList.append(beginWord)
+        wordMap = collections.defaultdict(list)
         for word in wordList:
             for i in range(len(word)):
                 pattern = word[:i] + "*" + word[i+1:]
-                neiMap[pattern].append(word)
+                wordMap[pattern].append(word)
         res = 1
-        visit = set([beginWord])
         q = deque([beginWord])
+        visit = set([beginWord])
         while q:
             for i in range(len(q)):
                 word = q.popleft()
-                if word == endWord:
+                if endWord == word:
                     return res
                 for j in range(len(word)):
-                    pattern = word[:j] +"*" + word[j+1:]
-                    for nei in neiMap[pattern]:
+                    pattern = word[:j] + "*" + word[j+1:]
+                    for nei in wordMap[pattern]:
                         if nei not in visit:
-                            visit.add(nei)
                             q.append(nei)
+                            visit.add(nei)
             res += 1
         return 0
 
 
 
-
-
-        

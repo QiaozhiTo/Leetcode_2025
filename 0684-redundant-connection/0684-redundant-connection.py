f294c391e624b9a871dd818ab9d15b72aad77bc9
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2026-01-11 15:01:10 -0500

    Time: 0 ms (100%), Space: 19.9 MB (6.43%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -5,18 +5,16 @@ class Solution:
         rank = [1] * (N + 1)
 
         def find(n):
-            p = n
-            while p != par[p]:
-                par[p] = par[par[p]]
-                p = par[p]
-            return p
-
-        def union(n1,n2):
+            res = n
+            while res != par[res]:
+                par[res] = par[par[res]]
+                res = par[res]
+            return res
+        def union(n1, n2):
             p1, p2 = find(n1), find(n2)
             if p1 == p2:
                 return False
-
-            if rank[p1] > rank[p2]:
+            if rank[p1] >= rank[p2]:
                 par[p2] = p1
                 rank[p1] += rank[p2]
             else:
@@ -25,4 +23,4 @@ class Solution:
             return True
         for n1, n2 in edges:
             if not union(n1, n2):
-                return [n1, n2]
+                return [n1,n2]

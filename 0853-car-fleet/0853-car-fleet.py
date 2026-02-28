933a5ed26e3fd3d86b48ec3bddb845bcd5367cda
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2026-02-28 14:19:09 -0500

    Time: 186 ms (43.36%), Space: 41.6 MB (49.31%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,8 +1,8 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         pair = [(p, s) for p, s in zip(position, speed)]
-        stack = []
         pair.sort(reverse = True)
+        stack = []
 
         for p, s in pair:
             stack.append((target - p)/s)

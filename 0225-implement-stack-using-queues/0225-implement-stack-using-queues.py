518cd11616c9436eaf816ab00df895edc95dac61
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2026-02-28 09:06:10 -0500

    Time: 0 ms (100%), Space: 19.6 MB (47.36%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -1,44 +1,22 @@
-# class MyStack:
-
-#     def __init__(self):
-#         self.queue = collections.deque()
-        
-
-#     def push(self, x: int) -> None:
-#         self.queue.append(x)
-#         for _ in range(len(self.queue) - 1):    
-#             self.queue.append(self.queue.popleft())    
-
-#     def pop(self) -> int:
-#         return self.queue.popleft()
-        
-
-#     def top(self) -> int:
-#         return self.queue[0]
-
 class MyStack:
 
     def __init__(self):
-        self.q = collections.deque()
+        self.q = deque()
         
+
     def push(self, x: int) -> None:
         self.q.append(x)
-        
-    def pop(self) -> int:
         for i in range(len(self.q) - 1):
             self.q.append(self.q.popleft())
+
+    def pop(self) -> int:
         return self.q.popleft()
         
 
     def top(self) -> int:
-        return self.q[-1]
+        return self.q[0]
         
 
-    def empty(self) -> bool:
-        return len(self.q) == 0
-        
-
-    
     def empty(self) -> bool:
         return len(self.q) == 0
         

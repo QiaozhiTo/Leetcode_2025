cc562fc801da246065cb9f1eb3631d3f28d3dafa
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2026-02-28 10:20:53 -0500

    Time: 3 ms (94.74%), Space: 20.4 MB (65.73%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -2,7 +2,7 @@ class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         stack = []
         for a in asteroids:
-            while stack and a< 0 and stack[-1] > 0:
+            while stack and stack[-1] > 0 and a < 0:
                 diff = stack[-1] + a
                 if diff < 0:
                     stack.pop()
@@ -11,28 +11,6 @@ class Solution:
                 else:
                     a = 0
                     stack.pop()
-            # if a => a is not 0, then append to stack
             if a:
                 stack.append(a)
         return stack
-
-# class Solution:
-#     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-#         stack = []
-#         for star in asteroids:
-#             while stack and star < 0 < stack[-1]:
-#                 if abs(star) > stack[-1]:
-#                     stack.pop()
-#                     continue # will skip the remaining code inside a loop for current iteration, so jump to 'stack.append(star)' 
-#                 elif abs(star) == stack[-1]:
-#                     stack.pop()
-#                 break 
-#             else:
-#                 stack.append(star)
-#         return stack
-
-# with continue or not example:
-# [-2,-2,-2,1,-2]
-
-
-

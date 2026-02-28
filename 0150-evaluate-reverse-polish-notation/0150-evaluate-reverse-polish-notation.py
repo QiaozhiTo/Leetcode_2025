ed937a30e629a58a8409c3aeba5f901ab02bbaef
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2026-02-28 10:28:12 -0500

    Time: 0 ms (100%), Space: 20.4 MB (85.62%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,40 +1,17 @@
-# class Solution:
-#     def evalRPN(self, tokens: List[str]) -> int:
-#         digits = []
-#         for ele in tokens:
-#             if ele not in '+-*/':
-#                 digits.append(int(ele))
-#             else:
-#                 num1 = digits.pop()
-#                 num2 = digits.pop()
-#                 if ele == '+':
-#                     digits.append(num2 + num1)
-#                 if ele == '-':
-#                     digits.append(num2 - num1)
-#                 if ele == '*':
-#                     digits.append(num2 * num1)
-#                 if ele == '/':
-#                     digits.append(int(num2 / num1))
-              
-#         return digits[0]
-
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-        for each in tokens:
-            if each == '+':
-                stack.append(stack.pop()+stack.pop())
-            elif each == '-':
-                a = stack.pop()
-                b = stack.pop()
-                stack.append(b-a)
-            elif each == '*':
-                stack.append(stack.pop()*stack.pop())
-            elif each == '/':
-                a = stack.pop()
-                b = stack.pop()
-                stack.append(int(b / a))
+        for token in tokens:
+            if token in '+*/-':
+                a, b = stack.pop(), stack.pop()
+                if token == '+':
+                    stack.append(a + b)
+                elif token == '-':
+                    stack.append(b - a)
+                elif token == '/':
+                    stack.append(int(b / a))
+                else:
+                    stack.append(a * b)
             else:
-                stack.append(int(each))
-        return stack[0]
-                
+                stack.append(int(token))
+        return stack[-1]

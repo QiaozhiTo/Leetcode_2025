f2d0dbf7f41196908ea75b4733fc7146690f1756
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2026-03-01 12:03:47 -0500

    Time: 1 ms (45.46%), Space: 21.1 MB (74.3%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.py
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.py
@@ -4,21 +4,23 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# class Solution:
-#     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-#         if not root: return TreeNode(val)
-#         curr = root
-#         while curr:
-#             if val < curr.val:
-#                 if not curr.left:
-#                     curr.left = TreeNode(val)
-#                     return root
-#                 curr = curr.left
-#             else:
-#                 if not curr.right:
-#                     curr.right = TreeNode(val)
-#                     return root
-#                 curr = curr.right
+
+#iteration 
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root: return TreeNode(val)
+        curr = root
+        while curr:
+            if val < curr.val:
+                if not curr.left:
+                    curr.left = TreeNode(val)
+                    return root
+                curr = curr.left
+            else:
+                if not curr.right:
+                    curr.right = TreeNode(val)
+                    return root
+                curr = curr.right
 # recursive 
 
 class Solution:

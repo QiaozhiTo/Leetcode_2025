031cced5b33cfd28c07dbb215dfd3346f734e928
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2026-03-01 11:15:40 -0500

    Time: 6 ms (19.27%), Space: 20.4 MB (64.47%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -4,31 +4,17 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-# Brute
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        if not root: return True
-        left = self.height(root.left)
-        right = self.height(root.right)
-        return (abs(left - right) <= 1 and 
-                self.isBalanced(root.left) and 
-                self.isBalanced(root.right))
-
-    def height(self, root):
-        if not root:return 0
-        return 1 + max(self.height(root.left), self.height(root.right))
+    
         
-
-# dfs
-class Solution:
-    def isBalanced(self, root: Optional[TreeNode]) -> bool:
         def dfs(root):
-            if not root: return [True, 0]
+            if not root:return [True,0]
             left = dfs(root.left)
             right = dfs(root.right)
-            balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
-            return [balanced, 1 + max(left[1], right[1])]
+            isBalanced = abs(left[1] - right[1]) <= 1 and left[0] and right[0]
+             
+            return [isBalanced, 1 + max(left[1], right[1])]
         return dfs(root)[0]
-
         
+         

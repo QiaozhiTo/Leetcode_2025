9beb37f65e81e667f134840f1edf8583e2321f27
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2026-03-22 18:47:00 -0400

    Time: 7579 ms (5.04%), Space: 23 MB (5.55%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -4,62 +4,16 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# brute force
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         if not root: return 0
-        left = self.maxHeight(root.left)
-        right = self.maxHeight(root.right)
+        left = self.height(root.left)
+        right = self.height(root.right)
         diameter = left + right
         sub = max(self.diameterOfBinaryTree(root.left), self.diameterOfBinaryTree(root.right))
         return max(sub, diameter)
 
-    def maxHeight(self, root):
+
+    def height(self, root):
         if not root: return 0
-        return 1 + max(self.maxHeight(root.left), self.maxHeight(root.right))
-
-# dfs
-class Solution:
-    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        self.res = 0
-        def dfs(node):
-            if not node: return 0
-            left = dfs(node.left)
-            right = dfs(node.right)
-            self.res = max(self.res, left + right)
-            return 1 + max(left, right)
-        dfs(root)
-        return self.res
-
-        
-'''
-class Solution:
-    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        # a global res
-        self.res = 0
-        # return height
-        def dfs(curr):
-            if not curr:return 0
-            left = dfs(curr.left)
-            right = dfs(curr.right)
-            self.res = max(self.res, left + right)
-            return 1 + max(left, right) #height of node curr
-        dfs(root)
-        return self.res
-
-class Solution:
-    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        #local res
-        res = 0
-        # return height
-        def dfs(curr):
-            if not curr:return 0
-            left = dfs(curr.left)
-            right = dfs(curr.right)
-            nonlocal res
-            res = max(res, left + right)
-            return 1 + max(left, right) #height of a node
-        dfs(root)
-        return res
-
-'''        
+        return 1 + max(self.height(root.left), self.height(root.right))

acc3b8b00f4dab3801c53e4d37c6d93aee43ed54
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2026-03-22 18:27:52 -0400

    Time: 0 ms (100%), Space: 19.2 MB (97.19%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
@@ -26,13 +26,13 @@ class Solution:
         return res
         
 
-# class Solution:
-#     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-#         res = []
-#         def dfs(node):
-#             if not node: return  
-#             dfs(node.left)
-#             dfs(node.right)
-#             res.append(node.val)
-#         dfs(root)
-#         return res
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        def dfs(node):
+            if not node: return  
+            dfs(node.left)
+            dfs(node.right)
+            res.append(node.val)
+        dfs(root)
+        return res

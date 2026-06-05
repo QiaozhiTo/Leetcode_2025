5d54e9d6a81d940007e11ff7848d6b8a81b84f4e
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2026-06-05 13:30:49 -0400

    Time: 299 ms (51.03%), Space: 48.5 MB (9.97%) - LeetHub

--- 0502-ipo/0502-ipo.py
+++ 0502-ipo/0502-ipo.py
@@ -1,14 +1,14 @@
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
         pairs = [[p, c] for p, c in zip(profits, capital)]
-        pairs.sort(key = lambda t : t[1])
-        maxHeap = [] # profit
+        pairs.sort(key = lambda p : p[1])
+        maxHeap = []
         i = 0
         for _ in range(k):
-            while i < len(pairs) and w >= pairs[i][1]:
+            while i < len(pairs) and  w >= pairs[i][1]:
                 heapq.heappush(maxHeap, -pairs[i][0])
                 i += 1
             if not maxHeap:
                 break
-            w += -heapq.heappop(maxHeap)
+            w += (-heapq.heappop(maxHeap))
         return w
